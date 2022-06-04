b861f051ebdd12d5628aa78a6dea43c2935b93fb
Hardik Arora <76209465+hardikkk20@users.noreply.github.com>
2022-06-04 19:47:01 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,3 +1,5 @@
+var colors=[{"button":"#ffffff","back": "#4d8ac7","lkey":"#cce5ff","lkeytext":"#5089be","fkey":"#a2d0f8","dkeytext":"#153c5a","calc":"#EBF5FF","backg":"#4d8ac7","ckeytext":"#ffffff"},
+            {"button":"#8c8c8c","back":"#ffffff","backg":"#000000","lkey":"#393939","ckeytext":"#000000","lkeytext":"#cfcfcf","fkey":"#535353","dkeytext":"#d9e3ea","calc":"#1b1b1b"}];
 
 function tap(id){
 console.log(document.getElementById("disptext").innerHTML);
@@ -19,3 +21,31 @@ function back(){
     var num2=num.substring(0,num.length-1);
     document.getElementById("disptext").innerHTML=num2;
 }
+
+function check(){
+    if(document.getElementById("dark").classList.contains("dark")){
+        theme(0);
+        document.getElementById("dark").classList.toggle("dark");
+        document.getElementById("dark").style.backgroundColor="black";
+        document.getElementById("dark").style.transform="rotate(-180deg)";
+    }
+    else{
+        theme(1);
+        document.getElementById("dark").classList.toggle("dark");
+        document.getElementById("dark").style.backgroundColor="white";
+        document.getElementById("dark").style.transform="rotate(0deg)";
+    }
+}
+function theme(index){
+      document.documentElement.style.setProperty("--button", (colors[index].button));
+      document.documentElement.style.setProperty("--back",(colors[index].back));
+      document.documentElement.style.setProperty("--lkey",(colors[index].lkey));
+      document.documentElement.style.setProperty("--lkeytext",(colors[index].lkeytext));
+      document.documentElement.style.setProperty("--fkey",(colors[index].fkey));
+      document.documentElement.style.setProperty("--dkeytext",(colors[index].dkeytext));
+      document.documentElement.style.setProperty("--calc",(colors[index].calc));
+      document.documentElement.style.setProperty("--ckeytext",(colors[index].ckeytext));
+      document.documentElement.style.setProperty("--backg",(colors[index].backg));
+  }
+  
+
