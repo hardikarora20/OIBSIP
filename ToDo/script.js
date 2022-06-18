8dbee6597fb71aec4927e073f73eaebc092c5e7c
Hardik Arora <76209465+hardikkk20@users.noreply.github.com>
2022-06-18 14:36:37 +0530

    Add files via upload

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,3 +1,5 @@
+var colors=[{"back":"#354f52","backli": "#52796f","li":"#c8d5b9","darker":"#c8d5b9a9","nav":"#253638"},
+            {"back":"#05445E","backli": "#377690","li":"#7dbcd6","darker":"#7dbcd6a9","nav":"#032635"}]
 var index=1;
 function add(){
     let text=document.getElementsByTagName("input")[0].value;
@@ -29,3 +31,21 @@ document.addEventListener('keypress', (event) => {
     }
   }, false);
   
+var i=1;
+function he(){
+    if(document.getElementsByTagName("p")[0].classList.contains("color")){
+        i=0;
+        document.getElementsByTagName("p")[0].classList.remove("color");
+    }
+    else{
+        i=1;
+        document.getElementsByTagName("p")[0].classList.add("color");
+    }
+    for(var k=0;k<3;k++){
+        document.getElementsByTagName("meta")[k].content=colors[i].nav;
+    }
+    document.documentElement.style.setProperty("--back", (colors[i].back));
+    document.documentElement.style.setProperty("--backli",(colors[i].backli));
+    document.documentElement.style.setProperty("--li",(colors[i].li));
+    document.documentElement.style.setProperty("--darker",(colors[i].darker));
+}
